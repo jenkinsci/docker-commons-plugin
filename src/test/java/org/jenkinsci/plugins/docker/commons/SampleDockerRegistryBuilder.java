df5f3e6f0d7539f3c47b5fcfa72eb952801ca45d
Jesse Glick <jglick@cloudbees.com>
2015-04-09 08:49:58 -0400

    Renamed field for consistency.

--- src/test/java/org/jenkinsci/plugins/docker/commons/SampleDockerRegistryBuilder.java
+++ src/test/java/org/jenkinsci/plugins/docker/commons/SampleDockerRegistryBuilder.java
@@ -12,30 +12,28 @@ import java.io.IOException;
  * @author Kohsuke Kawaguchi
  */
 public class SampleDockerRegistryBuilder extends Builder {
-    /**
-     * config.jelly should inline this.
-     * Not meant to be instantiated and referenced externally.
-     */
-    private final DockerRegistryEndpoint endpoint;
+
+    private final DockerRegistryEndpoint registry;
 
     @DataBoundConstructor
-    public SampleDockerRegistryBuilder(DockerRegistryEndpoint endpoint) {
-        this.endpoint = endpoint;
+    public SampleDockerRegistryBuilder(DockerRegistryEndpoint registry) {
+        this.registry = registry;
     }
 
-    public DockerRegistryEndpoint getEndpoint() {
-        return endpoint;
+    public DockerRegistryEndpoint getRegistry() {
+        return registry;
     }
 
     @Override
     public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
         // prepare the credentials to talk to this docker and make it available for docker you'll be forking
-        KeyMaterial key = endpoint.materialize(build);
+        KeyMaterial key = registry.materialize(build);
         try {
             // fork docker with appropriate environment to interact with this docker daemon
-            return launcher.launch().cmds("docker", "push", endpoint.imageName("user/image")).envs(key.env()).join() == 0;
+            return launcher.launch().cmds("docker", "push", registry.imageName("user/image")).envs(key.env()).join() == 0;
         } finally {
             key.close();
         }
     }
+
 }
