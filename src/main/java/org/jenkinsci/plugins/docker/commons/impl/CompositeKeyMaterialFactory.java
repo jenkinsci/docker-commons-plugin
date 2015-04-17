ba647ecbb4b3678a6331eaebac0a523d35b3ee59
Stephen Connolly <stephen.alan.connolly@gmail.com>
2015-04-17 10:38:03 +0100

    Make composition easier

--- src/main/java/org/jenkinsci/plugins/docker/commons/impl/CompositeKeyMaterialFactory.java
+++ src/main/java/org/jenkinsci/plugins/docker/commons/impl/CompositeKeyMaterialFactory.java
@@ -10,70 +10,77 @@ import java.io.IOException;
 import java.io.Serializable;
 
 /**
- * Composes two {@link org.jenkinsci.plugins.docker.commons.KeyMaterialFactory}s into one.
+ * Composes multiple {@link org.jenkinsci.plugins.docker.commons.KeyMaterialFactory}s into one.
  *
  * @author Kohsuke Kawaguchi
  */
 @Restricted(NoExternalUse.class)
 public class CompositeKeyMaterialFactory extends KeyMaterialFactory {
-    private final KeyMaterialFactory lhs;
-    private final KeyMaterialFactory rhs;
+    private final KeyMaterialFactory[] factories;
 
-    public CompositeKeyMaterialFactory(KeyMaterialFactory lhs, KeyMaterialFactory rhs) {
-        this.lhs = lhs;
-        this.rhs = rhs;
+    public CompositeKeyMaterialFactory(KeyMaterialFactory... factories) {
+        this.factories = factories == null || factories.length == 0
+                ? new KeyMaterialFactory[]{new NullKeyMaterialFactory()}
+                : factories.clone();
     }
 
     @Override
     public KeyMaterial materialize() throws IOException, InterruptedException {
-        KeyMaterial lhsMaterialized = lhs.materialize();
-        KeyMaterial rhsMaterialized = null;
-        
-        try {
-            rhsMaterialized = rhs.materialize();
-            EnvVars env = lhsMaterialized.env();
-            env.putAll(rhsMaterialized.env());
-    
-            return new CompositeKeyMaterial(env, lhsMaterialized, rhsMaterialized);
-        } catch (Exception e) {
-            try {
-                lhsMaterialized.close();
 
-                // TODO Java 7+ use chained exceptions
-                if (e instanceof IOException) {
-                    throw (IOException) e;
-                } else if (e instanceof InterruptedException) {
-                    throw (InterruptedException) e;
-                } else {
-                    throw new IOException("Error materializing credentials.", e);
+        KeyMaterial[] keyMaterials = new KeyMaterial[factories.length];
+        EnvVars env = new EnvVars();
+        try {
+            for (int index = 0; index < factories.length; index++) {
+                keyMaterials[index] = factories[index].materialize();
+                env.putAll(keyMaterials[index].env());
+            }
+            return new CompositeKeyMaterial(env, keyMaterials);
+        } catch (Exception e) {
+            for (int index = keyMaterials.length - 1; index >= 0; index--) {
+                try {
+                    if (keyMaterials[index] != null) {
+                        keyMaterials[index].close();
+                    }
+                } catch (IOException ioe) {
+                    // ignore
                 }
-            } finally {
-                if (rhsMaterialized != null) {
-                    rhsMaterialized.close();
-                }
+            }
+            // TODO Java 7+ use chained exceptions
+            if (e instanceof IOException) {
+                throw (IOException) e;
+            } else if (e instanceof InterruptedException) {
+                throw (InterruptedException) e;
+            } else {
+                throw new IOException("Error materializing credentials.", e);
             }
         }
     }
-    
+
     private static final class CompositeKeyMaterial extends KeyMaterial implements Serializable {
-        
+
         private static final long serialVersionUID = 1L;
 
-        private final KeyMaterial lhsMaterialized;
-        private final KeyMaterial rhsMaterialized;
+        private final KeyMaterial[] keyMaterials;
 
-        protected CompositeKeyMaterial(EnvVars envVars, KeyMaterial lhsMaterialized, KeyMaterial rhsMaterialized) {
+        protected CompositeKeyMaterial(EnvVars envVars, KeyMaterial... keyMaterials) {
             super(envVars);
-            this.lhsMaterialized = lhsMaterialized;
-            this.rhsMaterialized = rhsMaterialized;
+            this.keyMaterials = keyMaterials;
         }
 
         @Override
         public void close() throws IOException {
-            try {
-                lhsMaterialized.close();
-            } finally {
-                rhsMaterialized.close();
+            IOException first = null;
+            for (int index = keyMaterials.length - 1; index >= 0; index--) {
+                try {
+                    if (keyMaterials[index] != null) {
+                        keyMaterials[index].close();
+                    }
+                } catch (IOException ioe) {
+                    first = first == null ? ioe : first;
+                }
+            }
+            if (first != null) {
+                throw first;
             }
         }
     }
