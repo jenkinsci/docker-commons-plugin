8208416c0ae9a69f7478a3d16b74dfadf87e8551
Stephen Connolly <stephen.alan.connolly@gmail.com>
2015-04-17 11:10:28 +0100

    Simplify

--- src/main/java/org/jenkinsci/plugins/docker/commons/impl/ServerKeyMaterialFactory.java
+++ src/main/java/org/jenkinsci/plugins/docker/commons/impl/ServerKeyMaterialFactory.java
@@ -10,8 +10,6 @@ import org.kohsuke.accmod.restrictions.NoExternalUse;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -37,36 +35,31 @@ public class ServerKeyMaterialFactory extends KeyMaterialFactory {
 
     @Override
     public KeyMaterial materialize() throws IOException, InterruptedException {
-        final FilePath tempCredsDir;
         
+        EnvVars e = new EnvVars();
         if (credentials != null) {
             String key = credentials.getClientSecretKeyInPEM();
             String cert = credentials.getClientCertificateInPEM();
             String ca = credentials.getServerCaCertificateInPEM();
 
             if (key != null && cert != null && ca != null) {
-                tempCredsDir = new FilePath(baseDir, UUID.randomUUID().toString());
-                // these file names are defined by convention by docker
-                copyInto(tempCredsDir,"key.pem", key);
-                copyInto(tempCredsDir,"cert.pem", cert);
-                copyInto(tempCredsDir,"ca.pem", ca);
+                final FilePath tempCredsDir = new FilePath(baseDir, UUID.randomUUID().toString());
 
                 // protect this information from prying eyes
                 tempCredsDir.chmod(0600);
-            } else {
-                tempCredsDir = null;
+
+                // these file names are defined by convention by docker
+                copyInto(tempCredsDir, "key.pem", key);
+                copyInto(tempCredsDir,"cert.pem", cert);
+                copyInto(tempCredsDir,"ca.pem", ca);
+
+                e.put("DOCKER_TLS_VERIFY", "1");
+                e.put("DOCKER_CERT_PATH", tempCredsDir.getRemote());
+                return new ServerKeyMaterial(e, tempCredsDir);
             }
-        } else {
-            tempCredsDir = null;
         }
 
-        EnvVars e = new EnvVars();
-        if (tempCredsDir !=null) {
-            e.put("DOCKER_TLS_VERIFY", "1");
-            e.put("DOCKER_CERT_PATH", tempCredsDir.getRemote());
-        }
-
-        return new ServerKeyMaterial(e, tempCredsDir);
+        return new ServerKeyMaterial(e);
     }
 
     private void copyInto(FilePath dir, String fileName, String content) throws IOException, InterruptedException {
@@ -78,21 +71,41 @@ public class ServerKeyMaterialFactory extends KeyMaterialFactory {
     
     private static final class ServerKeyMaterial extends KeyMaterial {
 
-        private final FilePath tempCredsDir;
+        private final FilePath[] tempDirs;
 
-        protected ServerKeyMaterial(EnvVars envVars, FilePath tempCredsDir) {
+        protected ServerKeyMaterial(EnvVars envVars, FilePath... temporaryDirectories) {
             super(envVars);
-            this.tempCredsDir = tempCredsDir;
+            this.tempDirs = temporaryDirectories;
         }
 
         @Override
         public void close() throws IOException {
-            try {
-                if (tempCredsDir !=null) {
-                    tempCredsDir.deleteRecursive();
+            Throwable first = null;
+            if (tempDirs != null) {
+                for (FilePath tempDir : tempDirs) {
+                    try {
+                        tempDir.deleteRecursive();
+                    } catch (InterruptedException e) {
+                        first = first == null ? e : first;
+                    } catch (IOException e) {
+                        first = first == null ? e : first;
+                    } catch (RuntimeException e) {
+                        first = first == null ? e : first;
+                    } catch (Throwable e) {
+                        first = first == null ? e : first;
+                    }
                 }
-            } catch (InterruptedException e) {
-                throw new IOException(e);
+            }
+            if (first != null) {
+                if (first instanceof IOException) {
+                    throw (IOException) first;
+                } else if (first instanceof InterruptedException) {
+                    throw new IOException(first);
+                } else if (first instanceof RuntimeException) {
+                    throw (RuntimeException) first;
+                } else {
+                    throw new IOException("Error closing credentials.", first);
+                }
             }
         }
     }
