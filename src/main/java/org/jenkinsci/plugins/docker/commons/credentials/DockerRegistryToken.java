01b4f59879b10fd2bb868f3ea682bdaf6882473c
nakamura <nakamura@nulab.co.jp>
2017-09-24 03:54:29 +0900

    Remove endpoint protocol according to latest docker specific

--- src/main/java/org/jenkinsci/plugins/docker/commons/credentials/DockerRegistryToken.java
+++ src/main/java/org/jenkinsci/plugins/docker/commons/credentials/DockerRegistryToken.java
@@ -106,7 +106,7 @@ public final class DockerRegistryToken implements Serializable {
                             auths = json = new JSONObject();
                         }
                     }
-                    auths.put(endpoint.toString(), new JSONObject()
+                    auths.put(endpoint.getHost(), new JSONObject()
                             .accumulate("auth", getToken())
                             .accumulate("email", getEmail()));
                     
