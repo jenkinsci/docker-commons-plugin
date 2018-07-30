f2aa87f4e38385185c153a8a4e0a50b42f2711eb
Krassimir Valev <krassimir_valev@yahoo.com>
2018-07-30 08:45:06 +0200

    Inherit properties from the local docker config.json

--- src/main/java/org/jenkinsci/plugins/docker/commons/impl/RegistryKeyMaterialFactory.java
+++ src/main/java/org/jenkinsci/plugins/docker/commons/impl/RegistryKeyMaterialFactory.java
@@ -24,25 +24,34 @@
 
 package org.jenkinsci.plugins.docker.commons.impl;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import javax.annotation.Nonnull;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jenkinsci.plugins.docker.commons.credentials.KeyMaterial;
+import org.jenkinsci.plugins.docker.commons.credentials.KeyMaterialFactory;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
+
 import hudson.AbortException;
 import hudson.EnvVars;
 import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.TaskListener;
 import hudson.util.ArgumentListBuilder;
-import java.io.IOException;
-import java.net.URL;
-import javax.annotation.Nonnull;
-import org.jenkinsci.plugins.docker.commons.credentials.KeyMaterial;
-import org.jenkinsci.plugins.docker.commons.credentials.KeyMaterialFactory;
-import org.kohsuke.accmod.Restricted;
-import org.kohsuke.accmod.restrictions.NoExternalUse;
+import net.sf.json.JSONObject;
 
 /**
  * Logs you in to a Docker registry.
  */
 @Restricted(NoExternalUse.class)
 public class RegistryKeyMaterialFactory extends KeyMaterialFactory {
+
+    private static final String DOCKER_CONFIG_FILENAME = "config.json";
+    private static final String[] BLACKLISTED_PROPERTIES = { "auths", "credsStore" };
 
     private final @Nonnull String username;
     private final @Nonnull String password;
@@ -65,6 +74,26 @@ public class RegistryKeyMaterialFactory extends KeyMaterialFactory {
     @Override
     public KeyMaterial materialize() throws IOException, InterruptedException {
         FilePath dockerConfig = createSecretsDirectory();
+
+        // read the existing docker config file, which might hold some important settings (e.b. proxies)
+        FilePath configJsonPath = FilePath.getHomeDirectory(this.launcher.getChannel()).child(".docker").child(DOCKER_CONFIG_FILENAME);
+        if (configJsonPath.exists()) {
+            String configJson = configJsonPath.readToString();
+            if (StringUtils.isNotBlank(configJson)) {
+                launcher.getListener().getLogger().print("Using the existing docker config file.");
+
+                JSONObject json = JSONObject.fromObject(configJson);
+                for (String property : BLACKLISTED_PROPERTIES) {
+                    Object value = json.remove(property);
+                    if (value != null) {
+                        launcher.getListener().getLogger().print("Removing blacklisted property: " + property);
+                    }
+                }
+
+                dockerConfig.child(DOCKER_CONFIG_FILENAME).write(json.toString(), StandardCharsets.UTF_8.name());
+            }
+        }
+
         try {
             // TODO on Docker 17.07+ use --password-stdin
             EnvVars envWithConfig = new EnvVars(env);
